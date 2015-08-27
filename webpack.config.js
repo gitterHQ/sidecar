92472fd42083cb844d6e958b681937c77ead50d7
Eric Eastwood <contact@ericeastwood.com>
2015-08-27 06:23:23 -0500

    Remove CSS regex selectors (now: custom plugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
 var path = require('path');
 
+var postcss = require('postcss');
+var context = require('postcss-plugin-context');
 var autoprefixer = require('autoprefixer');
 var csswring = require('csswring');
 var nested = require('postcss-nested');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'sidecar.js',
-		library: 'sidecar',
-		libraryTarget: 'umd'
-	},
-	devtool: 'source-map',
-	module: {
-		loaders: [
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel'
-			},
-			{
-				test: /\.css$/,
-				loader: 'style-loader!css-loader!postcss-loader'
-			}
-		]
-	},
-	postcss: function () {
-		return [
-			nested,
-			autoprefixer,
-			csswring
-		];
-	}
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'sidecar.js',
+    library: 'sidecar',
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      }
+    ]
+  },
+  postcss: function () {
+    return [
+      nested(),
+      context({
+        'border-box': postcss.plugin('postcss-border-box', function (opts) {
+          opts = opts || {};
+          return function(css) {
+            css.eachRule(function(rule) {
+              var decl = postcss.decl({
+                prop: 'box-sizing',
+                value: 'border-box'
+              });
+              rule.prepend(decl);
+            });
+          };
+        })()
+      }),
+      autoprefixer(),
+      csswring()
+    ];
+  }
 };
